21ace7ca06984a29703c4417e7b1fd1835b27e2d
botnut71 <50204358+botnut71@users.noreply.github.com>
2019-05-05 21:30:12 +0200

    Add files via upload

--- main-working.rb
+++ main-working.rb
@@ -1,15 +1,19 @@
 require 'watir'
 
-File.open("combo.txt", "r").each_line do |line|
+puts "Link to playlist you want to follow: "
+playList = gets
 
+File.open("combo.txt", "r").each_line do |line|
+	
+  #Open combo list and split into variables
   data = line.split(/\t/)
   username, gay = data.map{|d| d.split(":")[0] }.flatten
   password, gay2 = data.map{|d| d.split(":")[1] }.flatten
 
-  puts "Username is: #{username} and Password: #{password} \n\n"
-  #sleep 1
+  puts "Username is: #{username} and Password: #{password}"
 
-  browser = Watir::Browser.new :firefox#, headless: true
+  #Start browser and go to login page
+  browser = Watir::Browser.new :chrome, headless: false
   browser.goto "https://accounts.spotify.com/sv/login/?continue=https:%2F%2Fwww.spotify.com%2Fse%2Faccount%2Foverview%2F&_locale=sv-SE"
 
   #Fill in form
@@ -19,31 +23,48 @@ File.open("combo.txt", "r").each_line do |line|
 
   #Click login
   puts "Clicking button.."
-  browser.button(:class => "btn btn-block btn-green ng-binding").click
+  browser.send_keys :enter
+  #browser.button(:class => ["btn", "btn-block", "btn-green", "ng-binding"]).click
 
   #Goto playlist and follow
+  sleep 0.3
   puts "Opening playlist.."
-  browser.goto "https://open.spotify.com/user/nathalie0217/playlist/5daFDq0pokAzjpCDWFBoTJ?si=2jYS7vUjRi6a04bUjc_kxg" #"https://open.spotify.com/playlist/6D16nv3xj5hT1tIuzngHUX?si=usLEPF1TQJWvEn8SEzjaEw"
+  browser.goto "#{playList}"
 
+  browser.element(:class => "contentSpacing").present?
+
+  #Check for ToS button
   puts "Checking if terms of service button is here.."
-
-  if browser.element(:class => "terms-of-service-modal__buttons-container").exists?
+  if browser.element(:class => "terms-of-service-modal__buttons-container").present?
     puts "ToS button found and pressed.."
-    browser.div(:class => "terms-of-service-modal__buttons-container").button(:class => "btn btn-green").when_present.click
+    browser.element(:class => "terms-of-service-modal__buttons-container").button(:class => ["btn", "btn-green"]).when_present.click
   else
   puts "No ToS button found.."
   end
 
-  #if browser.element(:class => "AdsContainer__inner").exists?
-  #  sleep 15
-  #end
+  #Check for privacy button
+  if browser.div(:class => ["btn", "btn-green"]).present?
+    puts "Pressing Privacy button.."
+    browser.div(:class => ["btn", "btn-green"]).click
+    sleep 0.3
+  end
 
-    if browser.div(:class => "horizontal-list tracklist-header__extra-buttons").button(:class => "btn btn-transparent btn--narrow").when_present.exists?
-      puts "Following playlist.."
-      browser.div(:class => "horizontal-list tracklist-header__extra-buttons").button(:class => "btn btn-transparent btn--narrow").when_present.click
-      sleep 0.5
-	  browser.close
-    else
+  #Check if already following
+  if browser.element(:class => "spoticon-heart-active-24").present?
+    puts "Already following this user..\n"
     browser.close
-    end
+    next
+  end
+  
+  #Click follow button
+  if browser.div(:class => ["horizontal-list", "tracklist-header__extra-buttons"]).button(:class => ["btn", "btn-transparent", "btn--narrow"])
+	browser.div(:class => ["horizontal-list", "tracklist-header__extra-buttons"]).button(:class => ["btn", "btn-transparent", "btn--narrow"]).click
+    puts "Following playlist..\n"
+    sleep 0.5
+	  browser.close
+  else
+    puts "Follow button not found, skipping\n"
+    browser.close
+    next
+  end
 end
